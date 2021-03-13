2439245a73d9225b59a01c11cfc59ba3b2f04ade
caio <caiomiguelmg@gmail.com>
2021-03-12 21:11:37 -0300

    app react-native

---
+++ src/screens/Favorites/index.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import { Platform, RefreshControl } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { request, PERMISSIONS } from 'react-native-permissions';
+import Geolocation from '@react-native-community/geolocation';
+
+import Api from '../../Api';
+
+import {
+    Container,
+    Scroller,
+
+    HeaderArea,
+    HeaderTitle,
+    SearchButton,
+
+    LocationArea,
+    LocationInput,
+    LocationFinder,
+
+    LoadingIcon,
+    ListArea
+} from './styles';
+
+import BarberItem from '../../components/BarberItem';
+
+import SearchIcon from '../../assets/search.svg';
+import MyLocationIcon from '../../assets/my_location.svg';
+
+
+export default () =>{
+
+    return(
+<Container>
+<Text>  Profile  </Text>
+</Container>
+
+    );
+}
